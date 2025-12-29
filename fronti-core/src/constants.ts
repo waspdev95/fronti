82eb44632a98c368d8159670cd28dc0e9474291e
Zafer <zaferyardimci1@gmail.com>
2025-12-29 21:18:28 +0300

    better

--- fronti-core/src/constants.ts
+++ fronti-core/src/constants.ts
@@ -4,7 +4,7 @@ export const WORKSPACE_CONFIG_FILE = 'workspace.json';
 export const NATIVE_HOST_DIR_NAME = 'native-host';
 
 // Version for smart manifest management
-export const NATIVE_HOST_VERSION = '1.0.6';
+export const NATIVE_HOST_VERSION = '1.0.7';
 
 export const REGISTRY_PATHS = {
   windows: `HKCU\\Software\\Google\\Chrome\\NativeMessagingHosts\\${NATIVE_HOST_NAME}`,
